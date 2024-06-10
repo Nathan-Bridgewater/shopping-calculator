b40cf58e304f4c5245fb678226e37661eeaf95fa
Nathan-Bridgewater <nathanbridgey99@gmail.com>
2024-06-10 08:25:24 +0100

    Started blog views

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,27 +13,28 @@ def register():
     db = get_db()
     error = None
 
-  if not username:
-    error = 'Username is required.'
-  elif not password:
-    error = 'Password is required.'
+    if not username:
+      error = 'Username is required.'
+    elif not password:
+      error = 'Password is required.'
 
-  if error is None: 
-    try:
-      db.execute(
-        "INSERT INTO user (username, password) VALUES (?, ?)",
-        (username, generate_password_hash(password)),   
-      )
-      db.commit()
-    except db.IntegrityError:
-      error = f"User {username} is already registered"
-    else:
-      return redirect(url_for('auth.login'))
+    if error is None: 
+      try:
+        db.execute(
+          "INSERT INTO user (username, password) VALUES (?, ?)",
+          (username, generate_password_hash(password)),   
+        )
+        db.commit()
+      except db.IntegrityError:
+        error = f"User {username} is already registered"
+      else:
+        return redirect(url_for('auth.login'))
+    
     flash(error)
   
-  return render_template('auth/register.html')
+  return render_template('register.html')
 
-@bp.route('login', methods=('GET', 'PSOT'))
+@bp.route('login', methods=('GET', 'POST'))
 def login():
   if request.method == 'POST':
     username = request.form['username']
@@ -56,7 +57,7 @@ def login():
     
     flash(error)
 
-  return render_template('auth/login.html')
+  return render_template('login.html')
   
 @bp.before_app_request
 def load_logged_in_user():
