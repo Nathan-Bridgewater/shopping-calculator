7fa94b2daaef5943a0d8282dab2ec51eafa7e872
Nathan-Bridgewater <nathanbridgey99@gmail.com>
2024-07-01 19:22:04 +0100

    Added subnet and updated gitignore

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -2,12 +2,28 @@ resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+//resource "aws_route_table" "route_table" {
+//  vpc_id = aws_vpc.main_vpc.vpc_id
+//
+//  route = 
+//}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners = ["amazon"]
   filter {
     name = "name"
-    values = ["al2023-ami-2023.4.20240611.0-kernel-6.1-arm64"]
+    values = ["al2023-ami-2023.5.20240624.0-kernel-6.1-x86_64"]
   }
 }
 
@@ -16,8 +32,11 @@ data "local_file" "public_ssh_key" {
 }
 
 resource "aws_instance" "web_server" {
-  ami = data.aws_ami.amazon_linux
+  ami = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  key_name = aws_key_pair.web_server_key_pair.key_name
+  subnet_id = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
 }
 
 resource "aws_key_pair" "web_server_key_pair" {
