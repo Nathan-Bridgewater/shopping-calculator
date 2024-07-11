d6489b9d70c1507cda406ad8cb54f79179b027e2
Nathan-Bridgewater <nathanbridgey99@gmail.com>
2024-07-11 08:54:01 +0100

    Enabled file upload basic

--- ui/app/routes.py
+++ ui/app/routes.py
@@ -1,5 +1,12 @@
 from app import app
+from flask import render_template, request, redirect, url_for
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return 'Hello World'
+  return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+  file = request.files['receipt']
+  print(file.read())
+  return redirect(url_for('index'))
